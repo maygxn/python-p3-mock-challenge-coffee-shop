41031c51272bc75ab1349d506f07b481aabca1a0
Meagan Truong <MeaganTruong1@yahoo.com>
2023-12-19 14:59:48 -0600

    solution 1

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,34 +1,106 @@
 class Coffee:
+    all = []
     def __init__(self, name):
         self.name = name
         
     def orders(self):
-        pass
+        coffee_orders = [] #array with relevant orders
+        for order in Order.all:
+            if(order.coffee == self):
+                coffee_orders.append(order)
+        return coffee_orders
     
     def customers(self):
-        pass
+        # the element that we want in the array - customer
+        # for loop - orders
+        # if statement
+        # list(set()) = to get UNIQUE COFFEE OBJECTS!!!!
+        return list(set([order.customer for order in self.orders()]))
     
     def num_orders(self):
-        pass
+        return len(self.orders())
     
     def average_price(self):
-        pass
+        #sum of all prices of orders (self.orders())
+        #number of orders (self.num_orders())
+        # for loop method
+        # total = 0
+        # for order in self.orders():
+        #     total += order.price
+        # return total/self.num_orders()
+        # list comprehension
+        total = sum([order.price for order in self.orders()])
+        return total/self.num_orders()
+    @property
+    def name(self):
+        return self._name
+
+    @name.getter
+    def name(self):
+        return self._name
+        
+    @name.setter
+    def name(self, val):
+        if not hasattr(self, 'name') and isinstance(val, str) and len(val) >= 3:
+            self._name = val
 
 class Customer:
+    all = []
     def __init__(self, name):
         self.name = name
         
     def orders(self):
-        pass
+        # list comprehension = need orders, iterate over Orders.all
+        # if order.customer == self
+        return [order for order in Order.all if order.customer == self]
+
     
     def coffees(self):
-        pass
+        # get all customer's coffees
+        # list(set()) = to get UNIQUE COFFEE OBJECTS!!!!
+        customer_coffees = []
+        for order in self.orders():
+            customer_coffees.append(order.coffee)
+        return list(set(customer_coffees))
+
     
     def create_order(self, coffee, price):
-        pass
+        new_order = Order(price = price, customer = self, coffee = coffee)
+        return new_order
     
+    @property
+    def name(self):
+        return self._name
+    
+    @name.getter
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name (self, val):
+        if isinstance(val, str) and 1 <= len(val) <= 15:
+            self._name = val
+
+
 class Order:
+    all = []
+    
     def __init__(self, customer, coffee, price):
         self.customer = customer
         self.coffee = coffee
-        self.price = price
+        self._price = price
+        Order.all.append(self)
+
+    @property
+    def price(self):
+        return self._price
+
+    @price.getter
+    def price(self):
+        return self._price
+    
+    @price.setter
+    # ERROR WITH price is immutable - assert 3.0 == 2.0 (Need to add "not" to and not hasattr)
+    def price(self, val):
+        if isinstance(val, float) and 1.0 <= val <= 10.0 and not hasattr(self, 'price'):
+            self._price = val
